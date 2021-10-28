6b176c5afc7ff97a3a2ede54e0fd62e8a672d942
Daria Voilakava <55460135+voilakava@users.noreply.github.com>
2021-10-28 11:18:16 +0300

    add node modules

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-function component() {
-  const element = document.createElement('div');
 
-  element.innerHTML = "Some text";
-  console.log("its works");
-  // const root = document.querySelector('#root');
-  return element; 
-}
+const name = React.createElement('h1', {}, 'My node');
 
-document.body.appendChild(component());
+const container = React.createElement('div', {}, name);
+
+ReactDOM.render(
+  container,
+  document.getElementById('root')
+);
+
+// function component() {
+//   const element = document.createElement('div');
+
+//   element.innerHTML = "Some text";
+//   console.log("its works");
+//   // const root = document.querySelector('#root');
+//   return element; 
+// }
+
+// document.body.appendChild(component());
 
 
 // console.log('Hello webpack 2');
